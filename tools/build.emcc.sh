ee8c938a35243dc66990797495ca3cf6c2d15c40
Micah Wylde <micah@micahw.com>
2013-09-09 23:33:19 -0700

    Added --typed-arrays 0 to emscripten command
    This fixes the issue with flexible structs

--- tools/build.emcc.sh
+++ tools/build.emcc.sh
@@ -29,4 +29,4 @@ cp -r $DIR/../include/www/* ../build/www/
 #TODO: Warn if subdirectories exist, since waf doesn't pick them up
 #TODO: Swap out $@ for targeted options?
 
-emcc $@ -O2 -o ../build/www/compiled.js -I $DIR/../include -I ../build -I ../include/ $DIR/../include/emscripten.pebble_os.c $DIR/../include/emscripten.pebble_dict.c $DIR/../include/SDL_prims.c *.c -s EXPORTED_FUNCTIONS="['_create_dict', '_add_string_to_dict', '_add_uint8_to_dict', '_add_uint16_to_dict', '_add_uint32_to_dict', '_add_int8_to_dict', '_add_int16_to_dict', '_add_int32_to_dict', '_send_dict_to_pebble', '_main']"
+emcc $@ -O0 --typed-arrays 0 -o ../build/www/compiled.js -I $DIR/../include -I ../build -I ../include/ $DIR/../include/emscripten.pebble_os.c $DIR/../include/emscripten.pebble_dict.c $DIR/../include/SDL_prims.c *.c -s EXPORTED_FUNCTIONS="['_create_dict', '_add_string_to_dict', '_add_uint8_to_dict', '_add_uint16_to_dict', '_add_uint32_to_dict', '_add_int8_to_dict', '_add_int16_to_dict', '_add_int32_to_dict', '_send_dict_to_pebble', '_main']"
